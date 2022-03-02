a78e428332744c2b9d7410d98eece77fb2d3a160
AhamedJINabil <johayeriqbal@gmail.com>
2022-03-02 11:08:42 +0600

    search section working

--- js/phone-khuji.js
+++ js/phone-khuji.js
@@ -2,7 +2,15 @@ const phoneSearch = () => {
     const searchField = document.getElementById('search-input');
     const searchFieldValue = searchField.value;
     searchField.value = '';
-    console.log(searchFieldValue);
 
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchFieldValue}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displaySearchResult(data.data))
 }
 
+
+const displaySearchResult = phones =>{
+    console.log(phones);
+
+}
